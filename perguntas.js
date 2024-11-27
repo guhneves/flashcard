55069e856d83ee194b24074a210c6bdb1d406d3a
guhneves <00001077325307sp@al.educacao.sp.gov.br>
2024-11-26 21:02:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Conhecimento Geral',
+    'Qual era o nome do primeiro imperador do Brasil?',
+    'Dom Pedro I'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Qual é o maior país em extensão terrtorial do mundo?',
+    'A Rússia'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Qual é o planeta mais próximo do sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Qual é o valor de π(pi) arredondado a duas casas decimais?',
+    '3,14'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Qual o rio mais longo do mundo?',
+    'Rio Amazonas'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Qual filme ganhou o Oscar de Melhor Filme em 1998?',
+    'Titanic'
+)
